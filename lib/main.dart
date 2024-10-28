192db4df297a2203ae491604390014f81921c783
Paul Jeconiah <pauljack1672@gmail.com>
2024-10-28 15:45:16 +0700

    mencoba membuat splash screen di native_splash.yaml dengan package flutter_native_splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:practice_flutter_2/pages/front_page.dart';
+import 'package:practice_flutter_2/pages/splash_screen.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Supabase.initialize(
-    url: 'https://xkuvfsdfscbxjbcpounf.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhrdXZmc2Rmc2NieGpiY3BvdW5mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg0ODk1NzUsImV4cCI6MjA0NDA2NTU3NX0.zPNbf6Bs-5YHRr5LqCWetjQzZaS7Rlj_q8WTN-F1814',
-  );
+
+  await Future.delayed(Duration(seconds: 5),);
+  FlutterNativeSplash.remove();
 
   runApp(const MyApp());
 }
---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:practice_flutter_2/components/image_component.dart';
+import 'package:practice_flutter_2/pages/front_page.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
